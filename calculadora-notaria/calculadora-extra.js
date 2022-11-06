65b3514a588c4925f822d329e07cb20e1e5ce5ed
Celio Rafael <cr2faria@gmail.com>
2022-11-06 16:14:51 -0300

    caculadora escritura

--- calculadora-notaria/calculadora-extra.js
+++ calculadora-notaria/calculadora-extra.js
@@ -1,7 +1,34 @@
 function calculaEscritura(valorNegocio)
 {
-    let totalEscritura = valorNegocio < 500 ? 1000 : 2000;
-    console.log(totalEscritura);
+    let totalEscritura = valorNegocio < 1268 
+    ? 298.50 
+    : valorNegocio< 4794
+    ? 446.07
+    : valorNegocio< 7991
+    ? 696.51
+    : valorNegocio< 15985
+    ? 995.02
+    : valorNegocio< 31970
+    ? 1344.97
+    : valorNegocio< 63940
+    ? 1595.47
+    : valorNegocio< 95910
+    ? 1893.99
+    : valorNegocio< 127880
+    ? 2243.93
+    : valorNegocio< 159850
+    ? 2542.51
+    : valorNegocio< 191820
+    ? 2844.43
+    : valorNegocio< 223790
+    ? 3190.97
+    : valorNegocio< 255760
+    ? 3492.94
+    : valorNegocio< 274721
+    ? 3842.89
+    : "fora da faixa";
+
+    console.log(`Total Escritura: R$ ${totalEscritura.toLocaleString('pt-BR')}`);
 }
 
-calculaEscritura(600);
+calculaEscritura(150000);
